152dc301a98420940b05a60d7dc39da265edbf5b
mariannyneris2004 <mariannyneris2004@gmail.com>
2023-07-15 14:06:52 -0300

    Melhoramento do código

--- introducao-logica-programacao/frutaria-promocao/src/Main.java
+++ introducao-logica-programacao/frutaria-promocao/src/Main.java
@@ -1,41 +1,24 @@
 import java.util.Scanner;
 
 public class Main {
-    public static double calculoFrutaPreco(Fruta fruta){
-        double precoFinalPorFruta, precoNormal = 1.45, precoPromocional = 1.25;
-
-        if (fruta.getQuantidade() > 10){
-            precoFinalPorFruta = fruta.getQuantidade() * precoPromocional;
-        } else {
-            precoFinalPorFruta = fruta.getQuantidade() * precoNormal;
-        }
-
-        return precoFinalPorFruta;
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Fruta fruta = new Fruta();
+
+        String[] frutasCadastradas = {"Pera", "Laranja", "Maçã", "Tomate", "Caqui"};
         double precoPorTipoDeFruta, precoFinal = 0;
+        double[] precosPorTipoDeFruta = new double[frutasCadastradas.length];
 
-        System.out.printf("FRUTARIA DA MARIA:\n\nFrutas promocionais:\nPera\nLaranja\nMaçã\nTomate\nCaqui");
-        System.out.println("\n(Digite 0 na quantidade caso queira encerrar a compra)");
-
-        do {
-            System.out.println("\nQual fruta deseja comprar? ");
-            fruta.setNome(scanner.next());
-            System.out.println("Qual a quantidade desejada? ");
+        System.out.printf("FRUTARIA DA MARIA:\n");
+        for (int i = 0; i < frutasCadastradas.length; i++) {
+            fruta.setNome(frutasCadastradas[i]);
+            System.out.println("Qual a quantidade desejada de " + frutasCadastradas[i] + "? ");
             fruta.setQuantidade(scanner.nextInt());
+            precoPorTipoDeFruta = Calculos.calculoFrutaPreco(fruta);
+            precosPorTipoDeFruta[i] = precoPorTipoDeFruta;
+        }
 
-            if(fruta.getQuantidade() !=0) {
-                precoPorTipoDeFruta = calculoFrutaPreco(fruta);
-
-                System.out.printf("A fruta %s sairá a R$%.2f.", fruta.getNome(), precoPorTipoDeFruta);
-
-                precoFinal += precoPorTipoDeFruta;
-            }
-
-        } while (fruta.getQuantidade() != 0);
+        precoFinal = Calculos.calculoTotalAPagar(precosPorTipoDeFruta);
 
         System.out.printf("A compra final é de R$%.2f", precoFinal);
 
